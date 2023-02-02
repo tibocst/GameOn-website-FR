ef3c71bbdcf3b0f15e4f9b2ef9c7dae5562e5476
tibocst <thibault.coste.233@gmail.com>
2023-02-02 22:31:57 +0100

    clode modal form terminé

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalBtnClose = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,10 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//close modal form
+
+modalBtnClose.addEventListener("click", function () {
+  modalbg.style.display = "none";
+  console.log("Réussi");
+}, false);
 
